238c668c7e15266b3603a8e5215bd77f773faafc
Damien Lecan <dev@dlecan.com>
2012-01-03 11:49:16 +0100

    Work In Progress - Autosave Tue Jan  3 10:49:16 GMT 2012

--- src/main/java/com/dlecan/sqli/wwcc/TimeLineBuilder.java
+++ src/main/java/com/dlecan/sqli/wwcc/TimeLineBuilder.java
@@ -2,4 +2,21 @@ package com.dlecan.sqli.wwcc;
 
 public final class TimeLineBuilder {
 
+	public TimeLineBuilder() {
+
+	}
+
+	public TimeLineBuilder forMonth(int month) {
+		return this;
+	}
+
+	public TimeLineBuilder withVisiteEnfant(int heureDebut, int minuteDebut,
+			int heureFin, int minuteFin) {
+		return this;
+	}
+
+	public TimeLine build() {
+		return null;
+	}
+
 }
