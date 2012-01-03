204e2cb7cf2a82af0d3e715e8fa7985f94a3dd14
Damien Lecan <dev@dlecan.com>
2012-01-03 14:27:01 +0100

    Work In Progress - Autosave Tue Jan  3 13:27:02 GMT 2012

--- src/main/java/com/dlecan/sqli/wwcc/QoSChecker.java
+++ src/main/java/com/dlecan/sqli/wwcc/QoSChecker.java
@@ -17,15 +17,15 @@ public class QoSChecker {
     private static final Logger LOGGER = LoggerFactory
             .getLogger(QoSChecker.class);
 
-    private static final byte ETAT_DISPONIBLE = 0; // 0
+    private static final byte ETAT_DISPONIBLE = 1 << 0; // 1
 
-    private static final byte ETAT_OUVERT_AUX_VISITES = 1 << 0; // 1
+    private static final byte ETAT_OUVERT_AUX_VISITES = 1 << 1; // 2
 
-    public static final byte ETAT_CHOCOLAT_BLANC = 1 << 1; // 2
+    public static final byte ETAT_CHOCOLAT_BLANC = 1 << 2; // 4
 
-    public static final byte ETAT_CHOCOLAT_NOIR = 1 << 2; // 4
+    public static final byte ETAT_CHOCOLAT_NOIR = 1 << 3; // 8
 
-    public static final byte ETAT_CHOCOLAT_LAIT = 1 << 3; // 8
+    public static final byte ETAT_CHOCOLAT_LAIT = 1 << 4; // 16
 
     private static final byte[] ETATS_CHOCOLAT = { ETAT_CHOCOLAT_BLANC,
             ETAT_CHOCOLAT_NOIR, ETAT_CHOCOLAT_LAIT };
