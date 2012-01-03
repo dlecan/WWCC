d9b04189d262341cab872323729b82e288328942
Damien Lecan <dev@dlecan.com>
2012-01-03 14:50:06 +0100

    Work In Progress - Autosave Tue Jan  3 13:50:06 GMT 2012

--- src/main/java/com/dlecan/sqli/wwcc/LecanDamien_JSG_Wonka.java
+++ src/main/java/com/dlecan/sqli/wwcc/LecanDamien_JSG_Wonka.java
@@ -40,6 +40,7 @@ public class LecanDamien_JSG_Wonka {
 
             NumberFormat percentInstance = NumberFormat.getPercentInstance(Locale.FRANCE);
             percentInstance.setMaximumFractionDigits(2);
+            percentInstance.setGroupingUsed(true);
             String qos = percentInstance.format(
                     resultats[5]);
             System.out
