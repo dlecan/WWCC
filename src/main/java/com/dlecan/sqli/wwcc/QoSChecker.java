4bf77d850bcc0ebc313559aadcf921a8127ed18e
Damien Lecan <dev@dlecan.com>
2012-01-03 14:26:01 +0100

    Work In Progress - Autosave Tue Jan  3 13:26:02 GMT 2012

--- src/main/java/com/dlecan/sqli/wwcc/QoSChecker.java
+++ src/main/java/com/dlecan/sqli/wwcc/QoSChecker.java
@@ -55,8 +55,6 @@ public class QoSChecker {
 
     private byte[] donnees;
 
-    private byte[] visites;
-
     public Object[] extractQoS(File qualityFile) {
         StopWatch stopWatch = new Slf4JStopWatch("extractQoS");
 
@@ -64,10 +62,9 @@ public class QoSChecker {
                 DUREE_FONCTIONNEMENT_THEORIQUE);
 
         donnees = new byte[NB_SECONDES_MOIS_11];
-        visites = new byte[NB_SECONDES_MOIS_11];
 
-        extraireIntervals(qualityFile);
         construireHeuresVisite();
+        extraireIntervals(qualityFile);
 
         Object[] resultats = mesureQoS();
         resultats[0] = DUREE_FONCTIONNEMENT_THEORIQUE;
@@ -93,11 +90,11 @@ public class QoSChecker {
         int offset = numJournee * NB_SECONDES_JOURNEE;
 
         // On indique les heures d'ouverture du matin pour cette journée
-        Arrays.fill(visites, offset + DEBUT_VISITE_MATIN, offset
+        Arrays.fill(donnees, offset + DEBUT_VISITE_MATIN, offset
                 + FIN_VISITE_MATIN, ETAT_OUVERT_AUX_VISITES);
 
         // Même chose pour l'après-midi
-        Arrays.fill(visites, offset + DEBUT_VISITE_APRES_MIDI, offset
+        Arrays.fill(donnees, offset + DEBUT_VISITE_APRES_MIDI, offset
                 + FIN_VISITE_APRES_MIDI, ETAT_OUVERT_AUX_VISITES);
     }
 
@@ -117,9 +114,9 @@ public class QoSChecker {
         for (i = 0; i < NB_SECONDES_MOIS_11; i++) {
 
             byte donnee = donnees[i];
-            byte visite = visites[i];
 
-            if (visite == ETAT_OUVERT_AUX_VISITES && donnee != ETAT_DISPONIBLE) {
+            if (contient(donnee, ETAT_OUVERT_AUX_VISITES)
+                    && !contient(donnee, ETAT_DISPONIBLE)) {
 
                 boolean auMoinsUnChocolatIndisponible = false;
 
@@ -128,7 +125,7 @@ public class QoSChecker {
 
                     byte etatChocolatEnCours = ETATS_CHOCOLAT[j];
 
-                    if ((donnee & etatChocolatEnCours) == etatChocolatEnCours) {
+                    if (contient(donnee, etatChocolatEnCours)) {
                         tempsChaqueChocolat[etatChocolatEnCours]++;
                         auMoinsUnChocolatIndisponible = true;
                     }
@@ -151,9 +148,9 @@ public class QoSChecker {
             }
         }
         Object[] resultats = new Object[6];
-        resultats[1] = tempsChaqueChocolat[2];
-        resultats[2] = tempsChaqueChocolat[4];
-        resultats[3] = tempsChaqueChocolat[8];
+        resultats[1] = tempsChaqueChocolat[ETAT_CHOCOLAT_BLANC];
+        resultats[2] = tempsChaqueChocolat[ETAT_CHOCOLAT_NOIR];
+        resultats[3] = tempsChaqueChocolat[ETAT_CHOCOLAT_LAIT];
 
         LOGGER.debug("Temps d'indisponibilite globale : {} secondes",
                 tempsAuMoinsUnChocolat);
@@ -255,5 +252,9 @@ public class QoSChecker {
 
     private byte typeChocolatToEtat(String typeChocolat) {
         return Chocolat.fromType(typeChocolat).getEtat();
+    }
+
+    boolean contient(int ensemble, int aVerifier) {
+        return (ensemble & aVerifier) == aVerifier;
     }
 }
